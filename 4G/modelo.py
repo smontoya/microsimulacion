e57751cee693166834961ea42a5af8b26fa9e6a0
Alfredo Solis <asolis02@gmail.com>
2016-12-06 11:21:44 -0300

    medicion modelo 4g

--- 4G/modelo.py
+++ 4G/modelo.py
@@ -9,9 +9,10 @@ class Antena4G(object):
         self.trafico = trafico
 
     def addToQueue(self, name):
-
+        total = 0
         print('%0.3f #%s Solicitando acceso 4G' % (self.env.now, name))
         duration = 0.01
+        total = total + duration
         yield self.env.process(self.hold(duration))
 
         with self.espacion_disponibles.request() as req:
@@ -20,107 +21,135 @@ class Antena4G(object):
 
             print('{} {} Source eNB -> UE: Control de la medida'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} UE, Source eNB / Source eNB, Target eNB, MME, Serving Gateway -> Paquete de datos'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> UE: Asignación de UL'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} UE -> Source eNB: Informe de medición'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} HO Decision'.format(self.env.now, name))
             duration = 0.001 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> Target eNB: Solicitud de traspaso'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Target eNB -> Source eNB: Solicitud de traspaso ACK'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> UE: Asignación de DL'.format(self.env.now, name))
             duration = 0.04 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> UE: Reconfiguración de la conexión RRC'.format(self.env.now, name))
             duration = 0.04 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> Target eNB: Transmisor de status SN'.format(self.env.now, name))
             duration = 0.04 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> Target eNB: Reenvío de datos'.format(self.env.now, name))
             duration = 0.04 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> Target eNB: Reenvío de datos'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} UE, Source eNB, Target eNB -> Sincronización'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Target eNB, Source eNB, UE -> Asignación de UL + TA para UE'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} UE, Source eNB, Target eNB -> Reconfiguración de la conexión RRC completo'.format(self.env.now, name))
             duration = 0.001 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} UE, Source eNB, Target eNB -> Paquete de datos'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Target eNB, MME, Serving Gateway -> Paquete de datos'.format(self.env.now, name))
             duration = 0.03 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Target eNB -> MME: Solicitud de interruptor de URL'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} MME -> Serving Gateway: Solicitud de actualización del plano del usuario'.format(self.env.now, name))
             duration = 0.04 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Serving Gateway, MME, Target eNB, Source eNB -> Marcador final'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Serving Gateway, MME, Target eNB -> Paquete de datos'.format(self.env.now, name))
             duration = 0.03 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Source eNB -> Target eNB: Marcador final'.format(self.env.now, name))
             duration = 0.01 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Serving Gateway -> MME: Solicitud de actualización del plano del usuario'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} MME -> Target eNB: Solicitud de interruptor de URL ACK'.format(self.env.now, name))
             duration = 0.02 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} MME -> Target eNB: Solicitud de interruptor de URL ACK'.format(self.env.now, name))
             duration = 0.002 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
 
             print('{} {} Target eNB -> Source eNB: Liberación de contexto de UE'.format(self.env.now, name))
             duration = 0.05 + self.trafico
+            total = total + duration
             yield self.env.process(self.hold(duration))
+            
+            print('{} {} Duración Modelo 4G: {}'.format(self.env.now, name, total))
 
     def hold(self, duration):
         yield self.env.timeout(duration)
